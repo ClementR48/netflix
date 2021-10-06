2358deba786a3fe2fa8fd29e001104c4b033b754
Clement <riera.clement@gmail.com>
2021-10-06 14:31:32 +0200

    fin de la premiere section page d'acceuil

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const inputElement = document.querySelector('form input');
-const span = document.querySelector('.adress-effect')
+const inputMail = document.querySelector('.input-label input');
+const labelMail = document.querySelector('.input-label label')
 
 
+/* Input animation au focus page principale */
+
+inputMail.addEventListener('focus', function(e) {
+  e.target.parentNode.parentNode.classList.add('active-input')
+})
+
+inputMail.addEventListener('blur', function(e) {
+  if(e.target.value === ""){
+    e.target.parentNode.parentNode.classList.remove('active-input')
+  }
+  
+})
+
+labelMail.addEventListener('click', function(e) {
+  e.target.parentNode.classList.add('active-input');
+  inputMail.focus();
+})
 
